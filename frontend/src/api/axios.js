84939c661819800b4f195eb3e1b4c4923ebd73e4
Siddhi-2005 <siddhipawar019@gmail.com>
2026-06-06 15:10:03 +0530

    Fix: Wrap baseURL string in quotes for production build

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: https://bda-team-management.onrender.com
+  baseURL: 'https://bda-team-management.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
